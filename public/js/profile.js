750972baebe4dc75eaf006c95e48561d2adee5c0
unknown <thomasjjohnson9311@gmail.com>
2023-05-15 23:34:33 -0500

    relationships

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,12 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, description }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -41,7 +40,3 @@ const delButtonHandler = async (event) => {
 document
   .querySelector('.new-project-form')
   .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
